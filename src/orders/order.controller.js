75ff820245118c5038feddbed1b03ee50480ad93
itsarasib <itsara.pliansart@gmail.com>
2024-11-15 18:03:51 +0700

    feat: create getOrderByEmail

--- src/orders/order.controller.js
+++ src/orders/order.controller.js
@@ -13,4 +13,18 @@ const createOrder = async (req, res) => {
   }
 };
 
-module.exports = { createOrder };
+const getOrdersByEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const orders = await Order.find({ email }).sort({ createdAt: -1 });
+    if (!orders) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("Error fetching orders by email", error);
+    res.status(500).json({ message: "Failed to fetch orders by email" });
+  }
+};
+
+module.exports = { createOrder, getOrdersByEmail };
--- src/orders/order.route.js
+++ src/orders/order.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createOrder } = require("./order.controller");
+const { createOrder, getOrdersByEmail } = require("./order.controller");
 
 const router = express.Router();
 
 //create order endpoint
 router.post("/", createOrder);
+
+router.get("/email/:email", getOrdersByEmail);
 
 module.exports = router;
